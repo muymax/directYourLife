d2c7fc32147c3def6aab7132b44d876d8300e472
Karsten Thacker <kthac535@gmail.com>
2020-06-14 16:56:15 -0600

    Third commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App(){
   return (
     <div>
       <Navbar/>
-      <br/>Hello world!! On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and 
+      <br/>Hello world!! Hello world again!!! On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and 
       demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the 
       pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through 
       ness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly 
