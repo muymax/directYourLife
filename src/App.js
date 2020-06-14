e376048582161009f0f2422dc570e2a76190a561
Karsten Thacker <kthac535@gmail.com>
2020-06-14 15:39:50 -0600

    First commit (start of project)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./components/Navbar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <br/>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and 
+      demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the 
+      pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through 
+      ness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly 
+      simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing 
+      prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. 
+      But in certain circumstances and owing to the claims of duty or the obligations of business it will 
+      frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore 
+      always holds in these matters to this principle of selection: he rejects pleasures to secure other greater 
+      sures, or else he endures pains to avoid worse pains."
+      <br/>
+
+      <br/>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and 
+      demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the 
+      pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through 
+      ness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly 
+      simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing 
+      prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. 
+      But in certain circumstances and owing to the claims of duty or the obligations of business it will 
+      frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore 
+      always holds in these matters to this principle of selection: he rejects pleasures to secure other greater 
+      sures, or else he endures pains to avoid worse pains."
+      <br/>
+
+      <br/>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and 
+      demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the 
+      pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through 
+      ness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly 
+      simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing 
+      prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. 
+      But in certain circumstances and owing to the claims of duty or the obligations of business it will 
+      frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore 
+      always holds in these matters to this principle of selection: he rejects pleasures to secure other greater 
+      sures, or else he endures pains to avoid worse pains."
+      "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and 
+      demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the 
+      pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through 
+      ness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly 
+      simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing 
+      prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. 
+      But in certain circumstances and owing to the claims of duty or the obligations of business it will 
+      frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore 
+      always holds in these matters to this principle of selection: he rejects pleasures to secure other greater 
+      sures, or else he endures pains to avoid worse pains."
+      "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and 
+      demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the 
+      pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through 
+      ness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly 
+      simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing 
+      prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. 
+      But in certain circumstances and owing to the claims of duty or the obligations of business it will 
+      frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore 
+      always holds in these matters to this principle of selection: he rejects pleasures to secure other greater 
+      sures, or else he endures pains to avoid worse pains."
+      <br/>
+
+      <br/>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and 
+      demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the 
+      pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through 
+      ness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly 
+      simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing 
+      prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. 
+      But in certain circumstances and owing to the claims of duty or the obligations of business it will 
+      frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore 
+      always holds in these matters to this principle of selection: he rejects pleasures to secure other greater 
+      sures, or else he endures pains to avoid worse pains."
+      "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and 
+      demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the 
+      pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through 
+      ness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly 
+      simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing 
+      prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. 
+      But in certain circumstances and owing to the claims of duty or the obligations of business it will 
+      frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore 
+      always holds in these matters to this principle of selection: he rejects pleasures to secure other greater 
+      sures, or else he endures pains to avoid worse pains."
+      "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and 
+      demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the 
+      pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through 
+      ness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly 
+      simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing 
+      prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. 
+      But in certain circumstances and owing to the claims of duty or the obligations of business it will 
+      frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore 
+      always holds in these matters to this principle of selection: he rejects pleasures to secure other greater 
+      sures, or else he endures pains to avoid worse pains."
+      <br/>
+      <Footer/>
     </div>
   );
 }
