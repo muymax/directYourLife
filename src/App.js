e704638902c7d278d7164a99eeee8c42524f9901
Karsten Thacker <kthac535@gmail.com>
2020-06-18 10:36:39 -0600

    about page commits

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import Navbar from "./components/layouts/Navbar";
@@ -6,24 +6,25 @@ import "./components/layouts/Navbar.css";
 import Footer from "./components/layouts/Footer";
 import "./components/layouts/Footer.css";
 // React Router Import
-import { Switch, Route} from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 //Import Pages
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import Contact from './components/pages/Contact';
-import PageNotFound from './components/pages/PageNotFound';
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import "./components/pages/About.css";
+import Contact from "./components/pages/Contact";
+import PageNotFound from "./components/pages/PageNotFound";
 
-function App(){
+function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/contact" component={Contact}/>
-        <Route component={PageNotFound}/>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={PageNotFound} />
       </Switch>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/components/pages/PageNotFound.js
+++ src/components/pages/PageNotFound.js
@@ -1,34 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class PageNotFound extends Component {
-    render() {
-        return (
-            <div>
-                <h2>PageNotFound page</h2>
-                <p>Lorem ipsum dolor sit amet, nunc sollicitudin tempor ullamcorper, a magna senectus wisi.
-                     Luctus blandit turpis id, id turpis vestibulum, minim amet suscipit turpis eu. Eget 
-                     egestas a vestibulum quisque sed nemo, justo ipsum vitae odio orci placerat, quisque 
-                     modi sapien lectus egestas. Pretium hac a hymenaeos at, id neque natoque lectus ultrices.
-                      Natoque pellentesque lacinia urna turpis consequuntur, leo nisl magnam, lorem amet, 
-                      gravida scelerisque, nunc eu integer tellus morbi lectus. Nullam adipiscing mauris in 
-                      varius lectus lorem, orci rutrum a aliquam, vestibulum leo nulla, vitae pulvinar eget. 
-                      Gravida rhoncus scelerisque ultricies erat leo ultrices, eget ridiculus, nonummy ac 
-                      adipiscing, ut ultrices nec congue, et aliquam consectetuer. Egestas neque molestie donec
-                      ue scelerisque sit, felis lacus malesuada praesent et, duis rhoncus mi vel natoque auctor 
-                      wisi. Quis lobortis, deserunt odio aliquet, adipiscing integer nullam elit mollis, faucibus
-                       ligula malesuada morbi, magnis donec felis arcu augue.
-                       Scelerisque viverra vivamus et lectus ratione, mollis rerum maecenas. Aliquam 
-                       consequuntur pellentesque mauris integer hendrerit elementum. Et rutrum. Habitasse 
-                       neque egestas morbi morbi torquent et, nullam mi quam ut sed aliquam, fringilla vitae
-                        metus. Donec mattis faucibus. Convallis fusce vel est sit convallis, diam mi diam 
-                        vulputate nonummy, est tellus, tristique nonummy per nibh. Et eget justo urna enim, 
-                        ridiculus expedita a, congue tempus augue in quam rhoncus, in et neque orci vitae sem,
-                         aliquam ligula. Massa vestibulum, sed iaculis tincidunt nec urna sit ac.
-                         <h2>Page Not Found </h2>
-                        </p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h2>PageNotFound page</h2>
+      </div>
+    );
+  }
 }
 
 export default PageNotFound;
